723dce0438cd6efef689ab17e44566bec555d387
y-hayamizu <yusaku.hayamizu@ieee.org>
2023-08-14 22:08:28 +0900

    minor changes

--- practice-a/livcam.bash
+++ practice-a/livcam.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-libcamera-vid --width 1280 --height 720 --bitrate 8192000 -n -t 0 -o /dev/stdout | ffmpeg -i - -vcodec mpeg4 -b:v 8M -acodec aac -f mpegts - | cefputstream ccnx:/stream -r 8 -b 1400 -t 3600 -e 3600
+libcamera-vid --width 1280 --height 720 --bitrate 4096000 -n -t 0 -o /dev/stdout | ffmpeg -i - -vcodec mpeg4 -b:v 4M -acodec aac -f mpegts - | cefputstream ccnx:/stream -r 4 -b 1400 -t 3600 -e 3600
