96131c3e2fddde70705088f248fbafff337627ae
y-hayamizu <yusaku.hayamizu@ieee.org>
2023-08-16 15:12:12 +0900

    modified the configurations of hostapd.conf

--- preparation/enable-wifi-ap.bash
+++ preparation/enable-wifi-ap.bash
@@ -7,6 +7,7 @@ sudo cp ./dhcpcd.conf /etc/dhcpcd.conf
 sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf
 sudo cp ./wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
+sudo systemctl stop hostapd.service
 sudo systemctl unmask hostapd.service
 sudo systemctl enable hostapd.service
 sudo systemctl start hostapd.service
